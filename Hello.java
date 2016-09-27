a283d1bffbe9288d5ae0e64bb7a72fb1e28309d0
Omkar <omkar.kelkar@coditas.com>
2016-09-27 18:52:02 +0530

    changed

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ class Hello
 public static void main(String args[])
 {
 System.out.print("HELLO WORLD");
-System.out.print{"Welcome to coditas");
+System.out.print("Welcome to coditas");
+System.out.println("GOOD BYE");
 }
 }
